5e087a0cd44366a2f671741cba7aa64f6f223405
AlexDays14 <al02954463@tecmilenio.mx>
2022-07-19 15:24:47 -0600

    secure email

--- helpers/emails.js
+++ helpers/emails.js
@@ -6,6 +6,7 @@ export async function confirmarCuenta(datos){
     const transport = nodemailer.createTransport({
         host: process.env.HOST_EMAIL,
         port: process.env.PORT_EMAIL,
+        secure: true,
         auth: {
           user: process.env.USER_EMAIL,
           pass: process.env.PASS_EMAIL
